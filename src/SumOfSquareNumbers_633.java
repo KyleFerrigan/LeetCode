c05ee8080f8b660d2f45f6ece9f59919a369a1d6
KyleFerrigan <work@kyleferrigan.com>
2024-06-17 11:21:45 -0500

    init

--- src/SumOfSquareNumbers_633.java
+++ src/SumOfSquareNumbers_633.java
@@ -28,6 +28,24 @@ public class SumOfSquareNumbers_633 {
         return false;
     }
 
+
+    // Smarter Brute Force
+    /*
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            int b =  c - (int)(a * a);
+            int i = 1, sum = 0;
+            while (sum < b) {
+                sum += i;
+                i += 2;
+            }
+            if (sum == b)
+                return true;
+        }
+        return false;
+    }
+    */
+
     // Too Slow
     // Precomputed Squares Method
     /*
